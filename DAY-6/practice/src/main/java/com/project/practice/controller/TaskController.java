12e46cc9af52524092e08f5fa864c8fcd3c308f7
Amrutavarshan L <118422092+varshan2534@users.noreply.github.com>
2024-08-02 00:26:51 +0530

    Add files via upload

---
+++ DAY-6/practice/src/main/java/com/project/practice/controller/TaskController.java
@@ -0,0 +1,45 @@
+package com.project.practice.controller;
+
+import com.project.practice.model.Task;
+import com.project.practice.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:3000")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    // Fetch tasks by user ID
+    @GetMapping("/{userId}")
+    public List<Task> getTasksByUserId(@PathVariable Long userId) {
+        return taskService.getAllTasksByUserId(userId);
+    }
+
+    // Fetch important tasks
+    @GetMapping("/important")
+    public List<Task> getImportantTasks() {
+        return taskService.getImportantTasks();
+    }
+
+    @PostMapping
+    public Task addTask(@RequestBody Task task) {
+        return taskService.saveTask(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        task.setId(id);
+        return taskService.saveTask(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTaskById(id);
+    }
+}
---
+++ DAY-6/practice/src/main/java/com/project/practice/service/TaskService.java
@@ -0,0 +1,68 @@
+package com.project.practice.service;
+
+import com.project.practice.model.Task;
+import com.project.practice.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    /**
+     * Fetches tasks based on the user ID.
+     * 
+     * @param userId The ID of the user.
+     * @return A list of tasks associated with the given user ID.
+     */
+    public List<Task> getAllTasksByUserId(Long userId) {
+        return taskRepository.findByUserId(userId);
+    }
+
+    /**
+     * Fetches important tasks.
+     * 
+     * @return A list of tasks marked as important.
+     */
+    public List<Task> getImportantTasks() {
+        return taskRepository.findByImportant(true);
+    }
+
+    /**
+     * Retrieves a task by its ID.
+     * 
+     * @param id The ID of the task.
+     * @return An Optional containing the task if found, otherwise empty.
+     */
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    /**
+     * Saves a task to the database. If the task has an ID, it will update the existing task.
+     * 
+     * @param task The task to save.
+     * @return The saved task.
+     */
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    /**
+     * Deletes a task by its ID.
+     * 
+     * @param id The ID of the task to delete.
+     */
+    public void deleteTaskById(Long id) {
+        if (taskRepository.existsById(id)) {
+            taskRepository.deleteById(id);
+        } else {
+            throw new IllegalArgumentException("Task with ID " + id + " does not exist.");
+        }
+    }
+}
